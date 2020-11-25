0a69075bc9f336052bd865b7c45c802f92f1c7cd
acn-support <bel.acn.tech@gmail.com>
2020-11-25 11:03:07 +0300

    fix overlay-scrollbar

--- src/js/vendors/overlay-scrollbar.js
+++ src/js/vendors/overlay-scrollbar.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    var scrollbar = $('.scrollbar');
-    if(scrollbar[0]) {
-        scrollbar.overlayScrollbars({
-            scrollbars: {
-                autoHide: 'l',
-                clickScrolling: true
-            },
-            className: 'os-theme-light'
-        });
-    }
+  const scrollbar = $('.scrollbar');
+  if (scrollbar[0]) {
+    scrollbar.overlayScrollbars({
+      className: 'os-theme-light',
+      scrollbars: {
+        autoHide: 'l',
+        clickScrolling: true
+      }
+    });
+  }
 });
